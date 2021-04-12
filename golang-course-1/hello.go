9798332c19f8a93fa1f7fc5da3ffce91a2b43aef
Leon <leonrubner2809@gmail.com>
2021-04-12 17:44:49 +0200

    golang file scanning commit

--- golang-course-1/hello.go
+++ golang-course-1/hello.go
@@ -87,4 +87,24 @@ func main() {
 		fmt.Println(value)
 	}
 
+	file, err := os.Open("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	err = file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if scanner.Err() != nil {
+		log.Fatal(scanner.Err())
+	}
+
 }
